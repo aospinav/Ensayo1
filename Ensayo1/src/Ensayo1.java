52e132b87341b38505dee42af4d620233b0f681f
aospinav <110316074+aospinav@users.noreply.github.com>
2022-08-01 07:13:54 -0500

    explicacion de github
    este es un ejemplo para el curso de cantera 2

---
+++ Ensayo1/src/Ensayo1.java
@@ -0,0 +1,9 @@
+import java.util.concurrent.SynchronousQueue;
+
+public class Ensayo1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hola campeon alejo");
+    }
+
+}
